0339902c1e91e8939f324abcca14cef8ad8fc88c
admin@xmy <1422019781@qq.com>
2023-08-19 12:32:53 +0800

    delete dyn head and main

--- dynamic_head.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-void func()
-{
-    printf("Hello Dynamic Linking\n");
-    sleep(-1);
-}
--- dynamic_main.c
+++
@@ -1,7 +0,0 @@
-#include "dynamic_head.h"
-
-int main()
-{
-    func();
-    return 0;
-}
